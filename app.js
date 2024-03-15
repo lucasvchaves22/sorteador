f563c6c16085b9e5946dfc16aa1e51f210b0e484
Lucas Vasconcelos <lucasvchaves22@gmail.com>
2024-03-14 22:41:12 -0300

    Adicionando validacoes aos imputs

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ function sortear () {
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
 
+    if (de >= ate) {
+        alert('Campo "Do número" deve ser inferior ao campo "Até o número", por favor verifique.');
+        return;
+    }
+
+    if (quantidade > (ate - de + 1)) {
+        alert('Campo "Quantidade de números" deve ter maior do que o intervalo entre os campos "Do número" e "Até o número", por favor verifique.');
+        return;
+    }
+
     let sorteados = [];
     let numero;
 
